c713d13783e238ef2cdb750466ceade062026a01
fatihmudab <fatihmuda618@gmail.com>
2026-04-13 18:34:21 -0700

    v1.0.0 chore: initialize project

---
+++ config/settings.py
@@ -0,0 +1,142 @@
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'django-insecure-g#tamd%p%r77bad0tsg86i6r_e%vnymjs^+z1gtqk*2a*=mi(z'
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*'] 
+
+
+# --- APPLICATION DEFINITION ---
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    
+    'rest_framework',
+    'rest_framework.authtoken', 
+    'corsheaders',              
+    'drf_yasg',                
+    'drf_spectacular',
+    'django_filters',         
+    'import_export',            
+]
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', 
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'config.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+
+# --- DATABASE ---
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cashier_db',       
+        'USER': 'postgres',            
+        'PASSWORD': '1234',   
+        'HOST': 'localhost',          
+        'PORT': '5432',               
+    }
+}
+
+
+# --- DJANGO REST FRAMEWORK CONFIG ---
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated', 
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
+}
+
+
+# --- CORS CONFIG ---
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+CORS_ALLOW_ALL_ORIGINS = True 
+
+
+# --- STATIC & MEDIA FILES ---
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
+# --- INTERNATIONALIZATION ---
+LANGUAGE_CODE = 'id-id' 
+TIME_ZONE = 'Asia/Jakarta'
+USE_I18N = True
+USE_TZ = True
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# --- CUSTOM USER MODEL ---
+# AUTH_USER_MODEL = 'user.CoreUser'
+
+
+# --- SWAGGER ---
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'KASIR API',
+    'DESCRIPTION': 'Dokumentasi API menggunakan Token Auth',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    'COMPONENT_SPLIT_PATCH': True,
+    'SECURITY': [
+        {
+            'TokenAuth': [],
+        }
+    ],
+    'APPEND_COMPONENTS': {
+        "securitySchemes": {
+            "TokenAuth": {
+                "type": "apiKey",
+                "in": "header",
+                "name": "Authorization",
+                "description": "Format: Token <your_token_key>"
+            }
+        }
+    },
+}
